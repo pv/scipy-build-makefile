4ef4eb4293811240e41247a525efaf0f61ec8532
Pauli Virtanen <pav@iki.fi>
2011-07-30 16:44:37 +0200

    Makefile additions

--- numpy/setupwin.py
+++ numpy/setupwin.py
@@ -1,5 +1,17 @@
 #!/usr/bin/env python
 import os
-os.environ['PATH'] += ";c:\\MinGW\\bin;c:\\Python25"
+import sys
+import subprocess
+
+os.environ['PATH'] += ";c:\\MinGW\\ccache-bin;c:\\MinGW\\bin"
 os.environ['MINGW'] = "c:\\MinGW"
-execfile('setup.py')
+os.environ['OPT'] = "-ggdb"
+
+if sys.version_info[0] >= 3:
+    def rep(x):
+        return x.replace('dist\\', '..\\..\\dist\\')
+else:
+    rep = lambda x: x
+
+args = [rep(x) for x in sys.argv[1:]]
+subprocess.call([sys.executable, 'setup.py'] + args)
