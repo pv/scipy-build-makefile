5e9649220c9bf71ba01bdb30aa6f49e8c09b5d2f
Pauli Virtanen <pav@iki.fi>
2012-05-12 18:07:39 +0200

    Add convenience script for running bisection searches in git

---
+++ bisectrun.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+"""
+bisectrun.py SCRIPT
+
+Run a script under Git bisect, rebuilding Numpy and arranging import paths
+first. If build fails, it is reported to Git as 'cannot test'.
+
+The script should raise an AssertionError if it fails, or run successfully.
+Any other errors raised are reported to Git as 'cannot test'.
+
+"""
+import sys
+import os
+import optparse
+
+def main():
+    p = optparse.OptionParser(usage=__doc__.strip())
+    options, args = p.parse_args()
+
+    if len(args) != 1:
+        p.error('no script file given')
+
+    script = os.path.abspath(args[0])
+
+    # -- Build and import
+    pyver = "%d.%d" % sys.version_info[:2]
+
+    os.chdir(os.path.abspath(os.path.dirname(__file__)))
+    ret = os.system("make clean build PYVER=%s" % pyver)
+
+    if ret != 0:
+        # Signal testing failure
+        print "TEST: cannot run: build failed"
+        sys.exit(125)
+
+    pth = os.path.abspath(os.path.join(os.path.dirname(__file__),
+        "dist", "linux", "lib", "python%s" % pyver, "site-packages"))
+    sys.path.insert(0, pth)
+
+    # -- Run test
+
+    try:
+        execfile(script)
+    except AssertionError, e:
+        print "TEST: failed:", e
+        sys.exit(1)
+    except BaseException, e:
+        print "TEST: cannot run:", e
+        sys.exit(125)
+
+    print "TEST: success"
+    sys.exit(0)
+
+if __name__ == "__main__":
+    main()
